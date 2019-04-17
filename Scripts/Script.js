dd7d659ca628d6ce0f98494de2910541407ffaac
Александр Макаров <sasha.makarov960@outlook.com>
2019-04-17 17:54:32 +0300

    Обновление

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,18 +1,12 @@
 $(document).ready(function($) {
-	// Обновление информации о лагере в Footer
-	{
-		console.log(Gallery);
-
-		// -- Телефоны
-		let _phones = document.getElementsByClassName('Contacts')[0].getElementsByTagName('h5')[0];
-		_phones.innerHTML = Information['phones'][0] + '<br>' + Information['phones'][1];
-
-		// -- Авторсие права
-		let _copyright = document.getElementsByClassName('Copyright')[0].getElementsByTagName('p')[0];
-		_copyright.innerHTML = Information["copyright"];
-	}
+	// Обновление информации о лагере в Footer (Телефоны и Авторсие права)
+	let _phones = document.getElementsByClassName('Contacts')[0].getElementsByTagName('h5')[0];
+	_phones.innerHTML = Information['phones'][0] + '<br>' + Information['phones'][1];
 	
-	// Отображение Menu в мобильной версии
+	let _copyright = document.getElementsByClassName('Copyright')[0].getElementsByTagName('p')[0];
+	_copyright.innerHTML = Information["copyright"];
+	
+	// Отображение Menu и Filter в мобильной версии
 	$('#Menu').on('click', function(event) {
 		event.preventDefault();
 
@@ -21,7 +15,6 @@ $(document).ready(function($) {
 		else { $('.SideBar .Filter').slideToggle(300); }
 	});
 
-	// Отображение Filter в мобильной версии
 	$('#Filter').on('click', function(event) {
 		event.preventDefault();
 
