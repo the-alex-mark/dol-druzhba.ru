781b3a1151f5f85e25a568105673bde40177890e
Александр Макаров <sasha.makarov960@outlook.com>
2019-04-18 20:20:41 +0300

    Обновление

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -23,4 +23,13 @@ $(document).ready(function($) {
 		else { $('.SideBar .Menu').slideToggle(300); }
 	});
 	
+	// Плавное перемещение до нужного ID
+	$(document).ready(function() {
+		$(".Smooth-Move").on("click", function (event) {
+			event.preventDefault();
+			var id = $(this).attr('href'), 
+			top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1200);
+		});
+	});
 });
